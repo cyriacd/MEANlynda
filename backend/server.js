d0917a543c832db5c20c2aea971fab1fe6bce5b1
Cyriac Domini <me@cyriacdomini.com>
2017-02-01 17:10:21 -0500

    more work on lynda tutorial

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongo = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+
 var database;
+
+var Message = mongoose.model('Message', {
+  msg: String
+});
+
 app.use(bodyParser.json());
 
 app.use(function(req,res,next){
@@ -11,16 +17,38 @@ app.use(function(req,res,next){
   next();
 })
 
+app.get('/api/message', getMessages)
+
 app.post('/api/message', function(req,res){
   console.log(req.body);
-  database.collection('messages').insertOne({"msg":req.body.msg});
-  res.status(200);
+
+  var message = new Message(req.body);
+  message.save(function(err){
+    if(err){
+      res.status(400);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({ status : 'error' }));
+      console.log(err);
+    }
+    res.status(200);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ status : 'ok' }));
+  });
+
 });
 
-mongo.connect("mongodb://localhost:27017/meantest", function(err,db){
+function getMessages(req,res){
+  Message.find({}).exec(function(err,result){
+    res.send(result)
+  });
+}
+
+
+mongoose.connect("mongodb://localhost:27017/meantest", function(err,db){
   if(!err){
     console.log("connected to mongo");
-    database = db;
+  }else{
+    console.log(err);
   }
 });
 
